c4d0c4c8343cd8c65664cb9b37358d655f8a19a8
Jake Soenneker <jake@soenneker.com>
2025-02-11 12:59:00 -0600

    Fluent

--- src/Registrars/TwilioClientUtilRegistrar.cs
+++ src/Registrars/TwilioClientUtilRegistrar.cs
@@ -1,27 +1,30 @@
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Soenneker.Twilio.Client.Abstract;
 
-namespace Soenneker.Twilio.Client.Registrars;
-
-/// <summary>
-/// An async thread-safe singleton for the Twilio client
-/// </summary>
-public static class TwilioClientUtilRegistrar
+namespace Soenneker.Twilio.Client.Registrars
 {
     /// <summary>
-    /// Adds <see cref="ITwilioClientUtil"/> as a singleton service. <para/>
+    /// An async thread-safe singleton for the Twilio client
     /// </summary>
-    public static void AddTwilioClientUtilAsSingleton(this IServiceCollection services)
+    public static class TwilioClientUtilRegistrar
     {
-        services.TryAddSingleton<ITwilioClientUtil, TwilioClientUtil>();
-    }
+        /// <summary>
+        /// Adds <see cref="ITwilioClientUtil"/> as a singleton service. <para/>
+        /// </summary>
+        public static IServiceCollection AddTwilioClientUtilAsSingleton(this IServiceCollection services)
+        {
+            services.TryAddSingleton<ITwilioClientUtil, TwilioClientUtil>();
+            return services;
+        }
 
-    /// <summary>
-    /// Adds <see cref="ITwilioClientUtil"/> as a scoped service. <para/>
-    /// </summary>
-    public static void AddTwilioClientUtilAsScoped(this IServiceCollection services)
-    {
-        services.TryAddScoped<ITwilioClientUtil, TwilioClientUtil>();
+        /// <summary>
+        /// Adds <see cref="ITwilioClientUtil"/> as a scoped service. <para/>
+        /// </summary>
+        public static IServiceCollection AddTwilioClientUtilAsScoped(this IServiceCollection services)
+        {
+            services.TryAddScoped<ITwilioClientUtil, TwilioClientUtil>();
+            return services;
+        }
     }
 }
